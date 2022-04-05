32e2a1b8b4721220df75e3b7e6aa85ad6d27439c
rykerdz <rykerdz18@gmail.com>
2022-04-05 15:06:19 +0100

    first commit

--- videoTester.py
+++ videoTester.py
@@ -4,6 +4,10 @@ import numpy as np
 from keras.models import model_from_json
 from keras.preprocessing import image
 
+
+
+base_dir = os.path.dirname(__file__)
+directory = os.fsencode("images")
 #load model
 model = model_from_json(open("fer.json", "r").read())
 #load weights
@@ -12,20 +16,17 @@ model.load_weights('fer.h5')
 
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-
-cap=cv2.VideoCapture(0)
-
-while True:
-    ret,test_img=cap.read()# captures frame and returns boolean value and captured image
-    if not ret:
-        continue
-    gray_img= cv2.cvtColor(test_img, cv2.COLOR_BGR2GRAY)
+for file in os.listdir(directory):
+    filename = os.fsdecode(file)
+    img=cv2.imread('images/'+filename)
+    print(filename)
+    gray_img= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
 
 
     for (x,y,w,h) in faces_detected:
-        cv2.rectangle(test_img,(x,y),(x+w,y+h),(255,0,0),thickness=7)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),thickness=7)
         roi_gray=gray_img[y:y+w,x:x+h]#cropping region of interest i.e. face area from  image
         roi_gray=cv2.resize(roi_gray,(48,48))
         img_pixels = image.img_to_array(roi_gray)
@@ -39,16 +40,6 @@ while True:
 
         emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
         predicted_emotion = emotions[max_index]
-
-        cv2.putText(test_img, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-
-    resized_img = cv2.resize(test_img, (1000, 700))
-    cv2.imshow('Facial emotion analysis ',resized_img)
-
-
-
-    if cv2.waitKey(10) == ord('q'):#wait until 'q' key is pressed
-        break
-
-cap.release()
-cv2.destroyAllWindows
+    
+    cv2.imwrite(predicted_emotion +'/' + filename + '.png', cv2.imread('images/'+filename))
+    
